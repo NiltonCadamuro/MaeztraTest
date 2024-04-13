f534f33107e8b92419fadb3baa4658474e0cc3a0
NiltonCadamuro <nilton.jrcadamuro@hotmail.com>
2024-04-13 19:55:35 -0300

    feat: banner outono finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+import BannerOutono from "@/components/BannerOutono";
 import MainBanner from "@/components/MainBanner";
 import Shelf from "@/components/Shelf";
-import ProductSummary from "@/components/Shelf/ProductSummary";
 
 export default function Home() {
   return (
@@ -37,6 +37,7 @@ export default function Home() {
       ]}
       />
       <Shelf />
+      <BannerOutono />
     </main>
   );
 }
