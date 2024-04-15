94a6c079e66eab0cca745b7de811484a0efe8f84
NiltonCadamuro <nilton.jrcadamuro@hotmail.com>
2024-04-15 15:39:59 -0300

    fix: adjust height

--- components/Footer/MenuAcourdeon/index.tsx
+++ components/Footer/MenuAcourdeon/index.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { MenuAcourdeonProps, MenuItem } from "./types";
 
 const MenuAcourdeon = ({ title, items }: MenuAcourdeonProps) => {
-  const [height, setHeight] = useState<number>(30);
+  const [height, setHeight] = useState<number>(32);
 
   return (
     <div
@@ -12,18 +12,18 @@ const MenuAcourdeon = ({ title, items }: MenuAcourdeonProps) => {
       className="w-full lg:max-w-[146px] overflow-hidden lg:!h-fit transition-all"
     >
       <div
-        onClick={() => setHeight(height === 30 ? items.length * 42 + 30 : 30)}
+        onClick={() => setHeight(height === 32 ? items.length * 30 + 32 : 32)}
         className="flex justify-between items-center"
       >
         <p className="text-textPrimary font-bold text-base leading-[18px]">
           {title}
         </p>
         <span className="text-textPrimary font-bold text-base leading-[18px] lg:hidden">
-          {height !== 30 ? "-" : "+"}
+          {height !== 32 ? "-" : "+"}
         </span>
       </div>
       {items && (
-        <div className="flex flex-col gap-6 mt-6">
+        <div className="flex flex-col gap-[10px] mt-[10px] lg:gap-6 lg:mt-6">
           {items.map((item: MenuItem, index: number) => (
             <a
               key={index}
